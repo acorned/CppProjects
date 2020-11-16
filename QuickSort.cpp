e888dca25d9c276a44cd4173c091496a6fb5a078
aleksandr-zheludkov <aleksandr_zheludkov@epam.com>
2020-11-16 14:39:31 +0300

    rename file

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -6,37 +6,33 @@ using namespace std;
 
 int partition (int arr[], int low, int high) 
 { 
-    int pivot = arr[high];    // pivot 
-    int i = (low - 1);  // Index of smaller element 
-  
+    int pivot = arr[high];
+    int i = (low - 1); 
     for (int j = low; j <= high- 1; j++) 
     { 
-        // If current element is smaller than or 
-        // equal to pivot 
         if (arr[j] <= pivot) 
         { 
-            i++;    // increment index of smaller element 
+            i++;   
             swap(arr[i], arr[j]); 
         } 
     } 
     swap(arr[i + 1], arr[high]); 
     return (i + 1); 
 } 
+
 void swap(int a, int b) {
     a = a + b;
     b = a - b;
     a = a - b;
 }
+
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        /* pi is partitioning index, arr[pi] is now
-           at right place */
         int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);  // Before pi
-        quickSort(arr, pi + 1, high); // After pi
+        quickSort(arr, low, pi - 1);  
+        quickSort(arr, pi + 1, high);
     }
 }
 
